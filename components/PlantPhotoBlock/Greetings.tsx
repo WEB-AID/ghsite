16f6b24733bc70deb0eb7753235a076162550209
WEB-AID <web.aid.dev@gmail.com>
2024-11-19 16:15:50 +0100

    plantblock ready

--- components/PlantPhotoBlock/Greetings.tsx
+++ components/PlantPhotoBlock/Greetings.tsx
@@ -14,7 +14,7 @@ export default function PlantPhotoBlock() {
                 width="0"
                 height="0"
                 sizes="100vw"
-                className="w-full h-35vh md:h-50vh min-[1280px]:h-70vh"
+                className="w-full max-[600px]:h-60 h-80 lg:h-96 min-[1280px]:h-[36rem]"
             />
             <p className="w-1/2 py-2 absolute top-4 left-10 md:w-4/5 md:mt-8 md:top-10 lg:mt-4 lg:top-22 text-white text-xs md:text-lg lg:text-3xl font-bold">
                 {t('plantPhotoText')}
